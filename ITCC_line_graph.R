449cf25e03736e0cb44e871a76841b87dfc30820
davemc528 <dcunnin1@tulane.edu>
2017-05-25 14:36:37 -0500

    Took away the background

--- ITCC_line_graph.R
+++ ITCC_line_graph.R
@@ -2,3 +2,5 @@ p <- ggplot(data = mouse_data_update,mapping = aes(x=post.surgery,y=mean_lum,gro
 #data taken from the mouse_data_updated file; need to list the luminescence, standard error, and treatment type all as variables
 p+geom_line()+geom_errorbar(ymax=mean_lum+lum_se,ymin=mean_lum-lum_se)+ylim(2,8)+ggtitle("intratibial cancer cell injection")+labs(x="Days post surgery",y="Mean luminescence (log)")
 #there's obviously more to play around with in this; i need to sit down with a ggplot book to get a better handle on the aesthetics
+p+geom_line(size=3)+geom_errorbar(ymax=mean_lum+lum_se,ymin=mean_lum-lum_se,size=3)+ylim(2,8)+ggtitle("intratibial cancer cell injection")+labs(x="Days post surgery",y="Mean luminescence (log)")+theme_classic()
+#takes away the grid background with theme_classic
